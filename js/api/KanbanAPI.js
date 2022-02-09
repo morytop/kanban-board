e7a4d8149bfb0ca85a179fc775ad2e0d075c4b63
Piotr Moryto <pmoryto@pmoryto-mbpro.local>
2022-02-09 16:34:48 +0100

    starting project

---
+++ js/api/KanbanAPI.js
@@ -0,0 +1,51 @@
+export default class KanbanAPI {
+    static getItems(columnId) {
+        const column = read().find(column => column.id === columnId);
+
+        if (!column) {
+            return [];
+        }
+
+        return column.items;
+    }
+
+    static insertItem(columnId, content){
+        const data = read();
+        const column = data.find(column => column.id === columnId);
+        const item = {
+            id: Math.floor(Math.random() * 1000000),
+            content
+        };
+
+        if (!column) {
+            
+        }
+    }
+}
+
+function read() {
+    const json = localStorage.getItem("kanban-data");
+
+    if (!json) {
+        return [
+            {
+                id: 1,
+                items: []
+            },
+            {
+                id: 2,
+                items: []
+            },
+            {
+                id: 3,
+                items: []
+            },
+        ];
+    }
+
+    return JSON.parse(json);
+}
+
+function save(data) {
+    localStorage.setItem("kanban-data", JSON.stringify(data));
+}
